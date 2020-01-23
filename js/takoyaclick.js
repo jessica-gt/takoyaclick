09ac48848fc580beed77b965e936e66b596b5a25
jessica-gt <58854245+jessica-gt@users.noreply.github.com>
2020-01-23 16:59:12 +0100

    Add files via upload

--- js/takoyaclick.js
+++ js/takoyaclick.js
@@ -21,10 +21,17 @@ let cptPoint=1;
 
 
 takoyaki.addEventListener('mousedown', function(e){
-	let r=Math.round(Math.random()*5)+1;
+	takoyaki.innerHTML='<img src="../Image/takoyakiCONTOUR2.png" alt="takoyaki">';
+	let r=Math.round(Math.random()*17)+1;
 	var plus=document.getElementById('plusScore'+r);
-	plus.innerHTML='+'+cptPoint;
-	plus.setAttribute('class','on');
+	plus.innerHTML='+'+cptPoint+'<img src="../Image/takoyaki" alt="takoyaki" height:"50px" width="50px">';
+	if (r<=6){	
+		plus.setAttribute('class','on1');
+	}else if (r>=13){	
+		plus.setAttribute('class','on3');
+	}else{	
+		plus.setAttribute('class','on2');
+	}
 })
 
 takoyaki.addEventListener('mouseup', function(e){
@@ -34,4 +41,17 @@ takoyaki.addEventListener('mouseup', function(e){
 		plusScore4.setAttribute('class','off');
 		plusScore5.setAttribute('class','off');
 		plusScore6.setAttribute('class','off');
+		plusScore7.setAttribute('class','off');
+		plusScore8.setAttribute('class','off');
+		plusScore9.setAttribute('class','off');
+		plusScore10.setAttribute('class','off');
+		plusScore11.setAttribute('class','off');
+		plusScore12.setAttribute('class','off');
+		plusScore13.setAttribute('class','off');
+		plusScore14.setAttribute('class','off');
+		plusScore15.setAttribute('class','off');
+		plusScore16.setAttribute('class','off');
+		plusScore17.setAttribute('class','off');
+		plusScore18.setAttribute('class','off');
+	takoyaki.innerHTML='<img src="../Image/takoyakiCONTOUR.png" alt="takoyaki">';
 })
